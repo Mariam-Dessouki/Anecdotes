7a132894a8c4be8f77117c099c5373d8bfb3a43a
Mariam-Dessouki <mariamdesouky97@gmail.com>
2022-02-26 21:05:45 +0200

    health

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const port = process.env.PORT || 3001;
 app.use("/db", middlewares, router);
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/health", (req, res) => {
   res.send("ok");
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
